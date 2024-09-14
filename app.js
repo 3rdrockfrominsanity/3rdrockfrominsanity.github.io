1453eeef817a77e7919d794805b204f81ee82375
3rdrockfrominsanity <chidubemileka22@gmail.com>
2024-09-14 20:57:59 +0100

    A small website project on gardening

---
+++ app.js
@@ -0,0 +1,5 @@
+ // Toggle description visibility
+ function toggleDescription() {
+    var container = document.querySelector('.plants-container');
+    container.classList.toggle('show-description');
+}
