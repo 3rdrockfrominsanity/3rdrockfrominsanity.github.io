d5c6ae9f25546727cc209a3825db971827def2ec
3rdrockfrominsanity <144026552+3rdrockfrominsanity@users.noreply.github.com>
2024-09-20 22:26:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
  // Toggle description visibility
- function toggleDescription() {
-    var container = document.querySelector('.plants-container');
+ function toggleDescription(button) {
+
+    const description = button.nextElementSibling;
+
+    if (description.classList.contains('description')) {
+        description.classList.toggle('active');
+    }
+
+    var container = document.querySelector('.plants-container');{
     container.classList.toggle('show-description');
+    }
 }
